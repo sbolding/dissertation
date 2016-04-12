c93db64022d7874c3df637941e341993ceca1c78
srb <simonrbolding@gmail.com>
2016-04-12 16:33:55 -0500

    minor changes

--- calculations/exact_solutions.py
+++ calculations/exact_solutions.py
@@ -5,6 +5,7 @@ from scipy.integrate import quadrature
 #integrate over a cell
 x_start = 0.
 x_end = 1./20.
+dx = x_end - x_start
 sigma = 1000
 psi_inc = 50
 q  = 1.0
@@ -13,5 +14,6 @@ psi_x1 = lambda x: (psi_inc*expn(2,sigma*x) + quadrature(lambda y:
     expn(1,sigma*y)*0.5*q,0,x,tol=1.e-12,maxiter=500)[0])
 psi_x2 = lambda x: psi_inc*expn(2,sigma*x) + 0.5*q/sigma*(1 - expn(2,sigma*x)) + 0.5*q/sigma
 phi_avg = 1/(x_end-x_start)*quadrature(lambda x: psi_x2(x),x_start,x_end,tol=1.E-12,maxiter=500)[0]
+phi_avg1 = 1/(x_end-x_start)*quadrature(lambda x: psi_x2(x),x_start+dx,x_end+dx,tol=1.E-12,maxiter=500)[0]
 
-print(phi_avg, psi_x2(0.5*(x_end+x_start)),psi_x2(x_end))
+print(phi_avg, psi_x2(x_end), phi_avg1)
